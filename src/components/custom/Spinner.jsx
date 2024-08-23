b1d0896cb8e33820a4eb85a48cc30de609aa454a
Akmal <akm.kxd@gmail.com>
2024-08-23 09:51:24 +0500

    FCAR-51 feat: implement search

--- src/components/custom/Spinner.jsx
+++ src/components/custom/Spinner.jsx
@@ -1,12 +1,12 @@
-const Spinner = () => {
+const Spinner = ({ width, height }) => {
   return (
     <div
       role='status'
-      className='absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2'
+      className='w-full h-full flex items-center justify-center'
     >
       <svg
         aria-hidden='true'
-        className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+        className={`inline w-${width} h-${height} text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`}
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
