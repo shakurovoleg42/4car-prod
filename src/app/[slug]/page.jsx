3834f9fbe26546aeaf867dc17d97da57bf73e3b5
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-08-23 10:53:03 +0500

    filters vis

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -2,7 +2,7 @@ import fetchService from '@/services/fetchs';
 import BrandPage from '@/components/templates/BrandPage/BrandPage';
 
 export default async function Brand({ params }) {
-  const manufacturers = await fetchService.getManufacturers();
+  const manufacturers = await fetchService.getManufacturersHome();
   const brand = await fetchService.getManufacturer(params.slug);
 
   return <BrandPage partners={manufacturers} brand={brand} />;
