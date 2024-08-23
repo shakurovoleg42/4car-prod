6b7ddc425dae3baa2b0374fb7bd42965adb0fc5c
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-08-23 15:35:57 +0500

    g

--- src/components/custom/Spinner.jsx
+++ src/components/custom/Spinner.jsx
@@ -1,12 +1,12 @@
-const Spinner = ({ width, height }) => {
+const Spinner = () => {
   return (
     <div
       role='status'
-      className='w-full h-full flex items-center justify-center'
+      className='absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2'
     >
       <svg
         aria-hidden='true'
-        className={`inline w-${width} h-${height} text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`}
+        className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
