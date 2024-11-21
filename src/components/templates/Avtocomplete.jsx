7bcf8ea8fc36f5d36a6470cdb36082aa8c9f2e6a
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-21 17:46:02 +0500

    fix filters

--- src/components/templates/Avtocomplete.jsx
+++ src/components/templates/Avtocomplete.jsx
@@ -248,7 +248,7 @@ export default function Playground({ filters }) {
                 <Autocomplete
                   {...defaultProps}
                   noOptionsText="Нет доступных вариантов"
-                  options={`${filterOptions.diameter}`}
+                  options={filterOptions.diameter}
                   id='disable-clearable5'
                   disableClearable
                   renderInput={(params) => (
