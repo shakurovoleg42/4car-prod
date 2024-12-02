64ac405064596460807ec039d1ef339f9439547b
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-12-02 09:42:45 +0500

    fix products chars & non-description text

--- src/components/templates/DescriptionModal.jsx
+++ src/components/templates/DescriptionModal.jsx
@@ -3,10 +3,15 @@ const DescriptionModal = ({product_fullDescription}) => {
     <>
       <div className='mt-10 mb-10'>
         <div className='font-forms flex flex-col gap-7'>
-          <div
-            className='mt-3'
-            dangerouslySetInnerHTML={{ __html: product_fullDescription }}
-          ></div>
+          
+          {product_fullDescription ? (
+            <div
+              className='mt-3'
+              dangerouslySetInnerHTML={{ __html: product_fullDescription }}
+            ></div>
+          ) : (
+            <p className="text-center font-bold text-3xl">Описание отсутствует</p>
+          )}
         </div>
       </div>
     </>
